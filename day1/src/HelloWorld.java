a67623d604b53707b417badfe4d269dda9c9c5fc
harshil9705 <hlakhani9705@gmail.com>
2025-01-28 07:51:43 +0530

    topic HelloWorld with argument

--- day1/src/HelloWorld.java
+++ day1/src/HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
 	public static void main(String[] argv){
 	System.out.println("Hello world");
-	System.out.println("Hello");
+	System.out.println("Hello"+ " " + argv[0] + " " + argv[1]);
 }
 }
