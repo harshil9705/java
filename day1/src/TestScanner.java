eb60489e8baf9b82700869b93caf11d7c48f54cb
harshil9705 <hlakhani9705@gmail.com>
2025-01-29 08:38:32 +0530

    topic: system class

---
+++ day1/src/TestScanner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class TestScanner {
+    public static void main(String[] argv){
+     Scanner sc = new Scanner(System.in);   
+     System.out.println("Enter 2 for multiplication");
+     double num1 = sc.nextDouble();
+     double num2= sc.nextDouble();
+
+        System.out.printf("result");
+    }
+}
